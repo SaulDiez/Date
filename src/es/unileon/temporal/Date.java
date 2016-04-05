0caa1e02d1c7b3150bf7625bc94609a89a5301bc
SaulDiez <sdiezf02@estudiantes.unileon.es>
2016-04-05 14:47:29 +0200

    Initial commit data.java

---
+++ src/es/unileon/temporal/Date.java
@@ -0,0 +1,36 @@
+
+public class Date{
+
+	private int dia;
+	private int mes;
+	private int año;
+
+	public Date(int dia, int mes,int año){
+	
+		dia=1;
+		mes=1;
+		año=1;
+	}
+
+
+}
+
+//Para la clase de miExcepcion
+
+package es.unileon...
+
+public class MiExcepcion extends Exception{
+
+	public MiExcepcion(String msg){
+		super(msg);
+	}
+}
+
+
+
+
+
+
+
+
+
